8b8d4e2950aa9ac6ff5ff10edfa006fe061922a6
SmartCoding51 <topstar51@outlook.com>
2022-05-26 08:16:06 +0900

    update vault contract

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -2,21 +2,60 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+import "./libraries/FixedPointMathLib.sol";
+
 contract Vault is ERC20, Ownable, ReentrancyGuard {
 
+    using FixedPointMathLib for uint256;
+    using SafeERC20 for IERC20;
+    
+    IERC20 public immutable asset;
+
+    event Deposit(address indexed _caller, address indexed _owner, uint256 _assets, uint256 _shares);
+
     constructor(
+        IERC20 _asset,
         string memory _name,
         string memory _symbol
-    ) ERC20(_name, _symbol) {}
+    ) ERC20(_name, _symbol) {
+        asset = _asset;
+    }
 
-    function deposit(uint256 _amount) external nonReentrant  {
+    function deposit(uint256 assets, address receiver) public virtual returns (uint256 shares) {
+        // Check for rounding error since we round down in previewDeposit.
+        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
 
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares);
     }
 
     function withdraw(uint256 _shares) external nonReentrant {
 
     }
+
+    function previewDeposit(uint256 assets) public view returns (uint256) {
+        return convertToShares(assets);
+    }
+
+    function totalAssets() public view returns (uint256) {
+    }
+
+    function convertToShares(uint256 assets) public view virtual returns (uint256) {
+        uint256 supply = totalSupply(); // Saves an extra SLOAD if totalSupply is non-zero.
+
+        return supply == 0 ? assets : assets.mulDivDown(supply, totalAssets());
+    }
+
+    function afterDeposit(uint256 assets, uint256 shares) internal {}
 }
