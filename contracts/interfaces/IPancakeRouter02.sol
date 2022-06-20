23f15af408e56a3d86561695ee0fa32b74d03897
SmartCoding51 <topstar51@outlook.com>
2022-06-21 02:30:35 +0900

    modify interface for PancakeswapRouter

--- contracts/interfaces/IPancakeRouter02.sol
+++ contracts/interfaces/IPancakeRouter02.sol
@@ -16,6 +16,15 @@ interface IPancakeRouter02 {
         uint deadline
     ) external returns (uint amountA, uint amountB, uint liquidity);
 
+    function addLiquidityETH(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+
     function removeLiquidity(
         address tokenA,
         address tokenB,
@@ -25,6 +34,15 @@ interface IPancakeRouter02 {
         address to,
         uint deadline
     ) external returns (uint amountA, uint amountB);
+
+    function removeLiquidityETH(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountToken, uint amountETH);
 
     function swapExactTokensForTokens(
         uint amountIn,
